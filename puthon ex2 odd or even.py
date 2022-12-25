f51e6109b675edd1cc87fb7b8b9ef993dce470c9
swathisekar28 <121334996+swathisekar28@users.noreply.github.com>
2022-12-25 14:13:31 +0530

    Add files via upload

---
+++ puthon ex2 odd or even.py
@@ -0,0 +1,7 @@
+e=int(input("enter a number to be checked :"))
+if(e%2==0):
+    print("The given number is even")
+else:
+    print("The given number is odd")
+    
+
---
+++ swapping python ex 2.py
@@ -0,0 +1,6 @@
+p=int(input("enter the value:"))
+q=int(input("enter the value:"))
+r=p
+p=q
+q=r
+print("the value after swapping",p,q)
